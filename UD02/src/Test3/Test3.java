410d49dd571e2ba023e1063fca51646cae52ec3a
adriaqf <adriaqf@gmail.com>
2022-04-05 17:57:53 +0200

    asdsasa

---
+++ UD02/src/Test3/Test3.java
@@ -0,0 +1,12 @@
+package Test3;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<6;i++)
+			System.out.print(i+ " ");
+		System.out.println("FIN DE PROGRAMA");
+	}
+
+}
